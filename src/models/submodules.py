abe33f8ec21a116f2b846aa83a6ca2885ced0953
Jun Guo <59831901+Joey0094@users.noreply.github.com>
2021-04-20 11:35:06 +0200

    Noticed Fault

--- src/models/submodules.py
+++ src/models/submodules.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+#### Batch Normalization should behind Conv or Linear layers! ####
 
 class Conv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
